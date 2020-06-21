c98d440730457e6950a7cb251b968943d17d1c1b
kamatayevrustem <61201673+kamatayevrustem@users.noreply.github.com>
2020-06-22 01:13:59 +0600

    Update json_xml.py

--- json_xml.py
+++ json_xml.py
@@ -6,55 +6,79 @@ import re
 #фукнция поиска топ 10 самых часто встречающихся в новостях слов длиннее 6 символов
 def show_top(list):
     data_word = list
-    str = ''
+    list_finish = []
     k = 0
-
     for i in data_word:
-        str = ''.join((str, i))
-
-    cnt = Counter(x for x in re.findall(r'[A-z\']{6,}', str))
+        list_finish.extend(i)
+    list_finish2 = ''.join(list_finish)
+    cnt = Counter(x for x in re.findall(r'[A-z\']{6,}', list_finish2))
     all_data = cnt.most_common()
-
     for item in all_data:
         if k<10:
             k+=1
             print(f'№{k}: {item[0]} повторяется {item[1]} раза')
 
-
 # функция парсинга XML файла
 def xml_parse(file):
-    list = []
-    file = file
-    tree = etree.parse(file)
-    root = tree.getroot()
-    for channel in root:
+    list_word = []
+    file_name = file
+    tree_name = etree.parse(file_name)
+    root_name = tree_name.getroot()
+    for channel in root_name:
         for data in channel:
             for include in data:
                 if include.tag == 'description':
-                    list.append(include.text)
+                    list_word.append(include.text)
     print('')
     print(f'Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов в XML:')
-    show_top(list)
+    show_top(list_word)
+
 
 # функция парсинга JSON файла
 def json_parse(file):
-    list = []
-    file = file
-    with open(file, 'r', encoding='utf-8') as myfile:
-        data=json.load(myfile)
-    for news in data:
-       for new in data[news]:
+    list_word = []
+    file_name = file
+    with open(file_name, 'r', encoding='utf-8') as myfile:
+        data_set=json.load(myfile)
+    for news in data_set:
+       for new in data_set[news]:
            if new=='channel':
-               for description in data[news][new]:
+               for description in data_set[news][new]:
                    if description == 'items':
-                       for descriptions in data[news][new]['items']:
-                           list.append(descriptions)
-    data = []
-    for item in list:
-        data.append(item['description'])
-    print('')
+                       for descriptions in data_set[news][new]['items']:
+                           list_word.append(descriptions['description'])
     print(f'Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов в JSON:')
-    show_top(data)
+    show_top(list_word)
 
 json_parse('file.json')
 xml_parse('xml_file.xml')
+
+
+
+# Результат программы:
+# D:\Anaconda3\python.exe "C:/Users/Рустем/Desktop/PYTHON/изучение/10 Работа с JSON и XML/json_full3.py"
+# Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов в JSON:
+# №1: Wilderness повторяется 23 раза
+# №2: Travel повторяется 17 раза
+# №3: Protea повторяется 15 раза
+# №4: Safaris повторяется 13 раза
+# №5: Marriott повторяется 12 раза
+# №6: International повторяется 9 раза
+# №7: Belmond повторяется 7 раза
+# №8: Hotels повторяется 7 раза
+# №9: Anantara повторяется 7 раза
+# №10: Africa повторяется 6 раза
+#
+# Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов в XML:
+# №1: Wilderness повторяется 23 раза
+# №2: Travel повторяется 17 раза
+# №3: Protea повторяется 15 раза
+# №4: Safaris повторяется 13 раза
+# №5: Marriott повторяется 12 раза
+# №6: International повторяется 9 раза
+# №7: Belmond повторяется 7 раза
+# №8: Hotels повторяется 7 раза
+# №9: Anantara повторяется 7 раза
+# №10: Africa повторяется 6 раза
+#
+# Process finished with exit code 0
