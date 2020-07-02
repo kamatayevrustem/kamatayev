98d366063fd942eb4f451ee02730b2ffbec0c7f6
kamatayevrustem <61201673+kamatayevrustem@users.noreply.github.com>
2020-07-02 22:52:50 +0600

    Update json_xml.py

--- json_xml.py
+++ json_xml.py
@@ -2,65 +2,58 @@ import json
 import xml.etree.ElementTree as etree
 from collections import Counter
 
-#фукнция поиска топ 10 самых часто встречающихся в новостях слов длиннее 6 символов
-def show_top(data_set):
-    data_word = data_set
-    k = 0
-    counter_data = Counter(data_word)
-    all_data = counter_data.most_common()
 
+def count_words(all_words):
+    counter_data = Counter(all_words)
+    all_data = counter_data.most_common()
+    k = 0
     for item in all_data:
-        if k<10:
-            k+=1
-            key,value = item
-            print(f'№{k}: слово "{key}" - повторятся {value} раз(а)')
+        if k < 10:
+            k += 1
+            key, value = item
+            print(f'№{k}: слово "{key}" - повторяется {value} раз(а)')
+    print('\n')
+
+
+# фукнция поиска топ 10 самых часто встречающихся в новостях слов длиннее 6 символов
+def show_top(work_text, type_file):
+    finish_list = []
+    for items in work_text:
+        if len(items) >= 6:
+            finish_list.append(items.lower())
+    print(f'Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов в {type_file}:')
+    count_words(finish_list)
+
 
 # функция парсинга XML файла
-def xml_parse(file_name):
+def xml_parse(xml_name):
     list_word = []
-    finish_list = []
-    file_itself = file_name
-    tree_name = etree.parse(file_itself)
+    tree_name = etree.parse(xml_name)
     root_name = tree_name.getroot()
     for channel in root_name:
         for data in channel:
             for include in data:
                 if include.tag == 'description':
                     list_word.append(include.text.split())
+    return list_word[0]
 
-    for items in list_word:
-        for words in items:
-            if len(words)>=6:
-                finish_list.append(words)
-
-    print(f'Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов в XML:')
-    show_top(finish_list)
-    print('\n')
 
 # функция парсинга JSON файла
-def json_parse(file_name):
-    file_itself = file_name
+def json_parse(json_name):
     list_of_data = []
-    finish_list = []
-
-    with open(file_itself, 'r', encoding='utf-8') as myfile:
-        data_set=json.load(myfile)
-
+    with open(json_name, 'r', encoding='utf-8') as myfile:
+        data_set = json.load(myfile)
     for item in data_set['rss']['channel']['items']:
         list_of_data.extend(item['description'].split())
+    return list_of_data
 
-    for items in list_of_data:
-        if len(items)>=6:
-            finish_list.append(items)
-
-    print(f'Топ 10 самых часто встречающихся в новостях слов длиннее 6 символов в JSON:')
-    show_top(finish_list)
-    print('\n')
 
 def main():
     # важно, новостью считается текст в items -> description, я взял реальные тексты статей без лишних тэгов, все перепроверил и работает отлично
-    json_parse('file.json')
-    xml_parse('xml_file.xml')
+    json_data = json_parse('file.json')
+    xml_data = xml_parse('xml_file.xml')
+    show_top(json_data, 'JSON')
+    show_top(xml_data, 'XML')
 
 
 if __name__ == '__main__':
