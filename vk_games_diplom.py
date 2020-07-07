9e679af31b98a3a0754a769df7a6c5df37803b63
kamatayevrustem <61201673+kamatayevrustem@users.noreply.github.com>
2020-07-07 23:01:20 +0600

    Update vk_games_diplom.py

--- vk_games_diplom.py
+++ vk_games_diplom.py
@@ -8,35 +8,31 @@ class VK:
     # начало программы
     def start(self):
         token = input('Введите TOKEN: ')
-        ids = int(person.get_id(token))
-        friend = person.friends(ids, token)
-        user_group = person.get_list_group(ids, token)
+        params_user = {
+            'access_token': token,
+            'v': '5.101'
+        }
+        ids = int(person.get_id(params_user))
+        friend = person.friends(ids, params_user)
+        user_group = person.get_list_group(ids, params_user)
         print(f'\n{len(user_group)} групп в ВК в которых состоит жертва: ', )
-        # print(*user_group)
-        friends_group = person.friends_group(friend, token)
+        friends_group = person.friends_group(friend, params_user)
         len_friends = len(friends_group)
         print(f'\n{len_friends} групп в ВК в которых состоят друзья жертвы: ')
-        # print(*friends_group)
         finish = person.exclusive_group(user_group, friends_group)
         print(f'\n{len(finish)} групп в ВК в которых состоит жертва, '
               f'но не состоит никто из его друзей: ')
-        # print(*finish)
-        person.group_info(finish, token)
+        groups_export = person.group_info(finish, params_user)
+        person.export_execlusive(groups_export)
         return True
 
     # функция чистки списка с друзьями от удаленных и забаненных
-    def clear_friends(self, actually, TOKEN):
+    def clear_friends(self, actually, params_user):
         print(len(actually), 'друзей у жертвы:')
         for i in actually:
-            params_user = {
-                'user_id': i,
-                'access_token': TOKEN,
-                'v': '5.101',
-            }
+            params_user['user_id'] = i
             URL_users_get = 'https://api.vk.com/method/users.get'
             print('.')
-            # print('clear_friends')
-            # time.sleep(1)
             repeat = True
             while repeat:
                 info = requests.get(URL_users_get, params_user)
@@ -58,17 +54,19 @@ class VK:
                     repeat = False
         return actually
 
+    # экспорт уникальных групп в JSON
+    def export_execlusive(self, json_list):
+        with open('groups.json', 'w', encoding="utf8") as f:
+            f.write(str(json_list))
+        print('я записал в groups.json')
+
     # получение ID жертвы, если введен никнейм, то переводив в ID
-    def get_id(self, TOKEN):
+    def get_id(self, params_user):
         victim = input('Введите ID или никнейм VK жертвы: ')
-        if victim.isdigit() == True:
+        if victim.isdigit():
             print("Вы ввели ID жертвы, продолжаем")
         else:
-            params_user = {
-                'user_ids': victim,
-                'access_token': TOKEN,
-                'v': '5.101',
-            }
+            params_user['user_ids'] = victim
             URL_users_get = 'https://api.vk.com/method/users.get'
             print('. получил ID по никнейму')
             info = requests.get(URL_users_get, params_user)
@@ -79,15 +77,10 @@ class VK:
         return victim
 
     # фукнция получения списка друзей жертвы
-    def friends(self, id_friends, TOKEN):
-        params_user = {
-            'user_id': id_friends,
-            'access_token': TOKEN,
-            'v': '5.101'
-        }
+    def friends(self, id_friends, params_user):
+        params_user['user_id'] = id_friends
         URL_friends_get = 'https://api.vk.com/method/friends.get'
         print('.')
-        # print('получаю список друзей')
         time.sleep(1)
         friends = requests.get(URL_friends_get, params_user)
         friends_list = friends.json()
@@ -96,22 +89,15 @@ class VK:
                 for include in item:
                     if include == 'items':
                         list = item[include]
-        list_finish = person.clear_friends(list, TOKEN)
-        # print(f'У жертвы {len(list2)} живых друзей: {list2}')
+        list_finish = person.clear_friends(list, params_user)
         return list_finish
 
     # получение списка групп
-    def get_list_group(self, guid, TOKEN):
+    def get_list_group(self, guid, params_user):
         array_friends = []
-        params_user = {
-            'user_id': guid,
-            'access_token': TOKEN,
-            'v': '5.101',
-            'fields': 'city'
-        }
+        params_user['user_id'] = guid
         URL_groups_get = 'https://api.vk.com/method/groups.get'
         print('.')
-        # print('получаю список групп')
         repeat = True
         while repeat:
             time.sleep(0.3)
@@ -132,10 +118,10 @@ class VK:
         return array_friends
 
     # получаем список групп друзей
-    def friends_group(self, friends_total, TOKEN):
+    def friends_group(self, friends_total, params_user):
         groups = []
         for i in friends_total:
-            group = person.get_list_group(i, TOKEN)
+            group = person.get_list_group(i, params_user)
             for item in group:
                 groups.append(item)
         return set(groups)
@@ -149,21 +135,16 @@ class VK:
         return users_group
 
     # информация о группе, точечная выгрузка
-    def group_info(self, items, TOKEN):
+    def group_info(self, items, params_user):
         json_list = []
         for jam in items:
-            params_user = {
-                'group_ids': jam,
-                'access_token': TOKEN,
-                'v': '5.101',
-                'fields': 'members_count'
-            }
+            params_user['group_ids'] = jam
+            params_user['fields'] = 'members_count'
             URL_groups_getById = 'https://api.vk.com/method/groups.getById'
             print('.')
             time.sleep(0.3)
             group_info = requests.get(URL_groups_getById, params_user)
             info = group_info.json()
-            # print(f'group_info: {info}')
             for item in info['response']:
                 name = item['name']
                 gid = item['id']
@@ -171,9 +152,7 @@ class VK:
                 raw = {"name": f'{name}', "gid": f'{gid}',
                        "members_count": f'{members_count}'}
                 json_list.append(raw)
-        with open('groups.json', 'w', encoding="utf8") as f:
-            f.write(str(json_list))
-        print('я записал в groups.json')
+        return json_list
 
 
 person = VK()
